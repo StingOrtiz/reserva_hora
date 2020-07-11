6fec377c5ac164a4fc3d02d8d7d67e13f640d08d
StingOrtiz <stingortiz@gmail.com>
2020-07-11 16:14:29 -0400

    'prod'

--- src/app/cuerpo/cuerpo.module.ts
+++ src/app/cuerpo/cuerpo.module.ts
@@ -2,18 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {CuerpoRoutingModule,routecomponent,providerroute} from 'src/app/cuerpo/cuerpo-routing.module';
 import {MatTableModule} from '@angular/material';
+import { UsuariosComponent} from './usuarios/usuarios/usuarios.component';
+import { UsuariosCrearComponent} from './usuarios/usuarios-crear/usuarios-crear.component';
+import { UsuariosEditarComponent} from './usuarios/usuarios-editar/usuarios-editar.component';
+
+
+
 
 
 
 
 @NgModule({
-  declarations: [routecomponent,],
+    
+  declarations: [routecomponent,    UsuariosComponent,
+    UsuariosCrearComponent,
+    UsuariosEditarComponent],
     
   providers:[providerroute],
   imports: [
     CommonModule,
     CuerpoRoutingModule,
     MatTableModule,
+
   ]
 })
 export class CuerpoModule { }
