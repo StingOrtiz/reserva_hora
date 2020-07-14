0bc13cf18a58367470ec760ad4efe4e0c14688cb
StingOrtiz <stingortiz@gmail.com>
2020-07-13 22:10:42 -0400

    cambio registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -63,7 +63,7 @@ export class RegisterComponent implements OnInit {
             .subscribe(
                 data => {
                     this.alertService.success('Registration successful', true);
-                    this.router.navigate(['/logged']);
+                    this.router.navigate(['']);
                 },
                 error => {
                     this.alertService.error(error);
